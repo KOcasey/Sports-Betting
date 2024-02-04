13c8d686d1709fe943eda24d89d172f58c5f3323
Casey Cooper <caseykcooper14@gmail.com>
2024-02-04 15:20:11 -0700

    edit port for ec2

--- app.py
+++ app.py
@@ -75,4 +75,4 @@ def predict():
         return render_template("index.html", prediction_text='The Predicted Total Result is: ' + str(prediction))
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    app.run(debug=True, host='0.0.0.0', port=8000)
